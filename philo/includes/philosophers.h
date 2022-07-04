5f1466d698ecb99e2ca1b314bc18779dc34beee2
Baptist Levrel <blevrel@2A7.42angouleme.fr>
2022-07-04 08:14:37 +0200

    first push

---
+++ philo/includes/philosophers.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blevrel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/26 17:38:49 by blevrel           #+#    #+#             */
+/*   Updated: 2022/06/26 18:11:02 by blevrel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+
+
+#endif
